d6a481329cddf7407b6e39f397745b5a0c1c458d
wolf811 <wolf811@gmail.com>
2022-06-18 23:05:59 +0300

    end profile

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,12 @@
-$(document).ready(function() {
-    console.log('Ghbdtvndkjf');
+const biEyeSlashEls = document.querySelectorAll('.bi-eye-slash');
+biEyeSlashEls.forEach(el => {
+    el.addEventListener('click', event => {
+        if (event.target.classList.contains(el)) {
+            event.target.classList.toggle(el);
+            // event.target.classList.add('bi-eye');
+        } else {
+            // event.target.classList.add(el);
+            event.target.classList.toggle('bi-eye');
+        }
+    })
 })
